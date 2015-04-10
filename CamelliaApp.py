b8c1e7f28c0937671e4c6b840217a99ce210ad42
Nathaniel L. Hills <nate.hills@cslab05.wheaton.edu>
2015-04-09 20:00:10 -0500

    Made a starter NUI

---
+++ CamelliaApp.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
+from kivy.uix import *
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.tabbedpanel import TabbedPanel
+
+
+
+
+class CamelliaWindow(TabbedPanel):
+    pass
+
+
+class CamelliaApp(App):
+    def build(self):
+        return CamelliaWindow()
+
+
+
+if __name__ == '__main__':
+    CamelliaApp().run()
