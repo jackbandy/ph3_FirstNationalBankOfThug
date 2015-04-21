c7036898494734667a4ee738dd57089cbe8d2413
Jack Bandy <jackbandy@icloud.com>
2015-04-21 15:43:19 -0500

    Implemented plotMesh in plotter.py

--- PlotTests.py
+++ PlotTests.py
@@ -17,22 +17,22 @@ class PlotTests(unittest.TestCase):
   x_vels = [Function.constant(5), Function.constant(1)]
   y_vels = [Function.constant(6), Function.constant(3)]
   outflows = [SpatialFilter.matchingX(4), SpatialFilter.matchingY(4)]
-  dims = [2.0, 8.0]
-  elems = [2,2]
+  dims = [8.0, 8.0]
+  elems = [4,4]
   form = c.main(3, inflows, x_vels, y_vels, outflows, dims, elems)
 
-  def testu1(self):
-    self.plter.plotU1(self.form)
-  def testu2(self):
-    self.plter.plotU2(self.form)
-  def testp(self):
-    self.plter.plotP(self.form)
-  def teststream(self):
-    self.plter.plotStream(self.form)
-#  def testmesh(self):
-#    plotter.plotMesh(form)
-  def testerror(self):
-    self.plter.plotError(self.form,True)
+#  def testu1(self):
+#    self.plter.plotU1(self.form)
+#  def testu2(self):
+#    self.plter.plotU2(self.form)
+#  def testp(self):
+#    self.plter.plotP(self.form)
+#  def teststream(self):
+#    self.plter.plotStream(self.form)
+  def testmesh(self):
+    self.plter.plotMesh(self.form)
+#  def testerror(self):
+#    self.plter.plotError(self.form,True)
 
 # Run the tests:
 if (__name__ == '__main__'):
