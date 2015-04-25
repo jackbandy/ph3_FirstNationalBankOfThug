7dd51a910a08b2a03e95168ecf4beb3cfc9d9147
derekschlabach <derekschlabach@gmail.com>
2015-04-24 21:55:58 -0500

    mved stuff

--- program/FormCreator.py
+++ program/FormCreator.py
@@ -1,75 +1,62 @@
 from PyCamellia import *
-from State import *
-from PostSolve import *
 from string import *
 import os
 
 #takes the inputs from the GUI and returns a formulation with all conditions attached
-class FormCreator(State):
-
-    def __init__(self, poly, inflows, inflow_x_vels, inflow_y_vels, outflows,  dims, elems,re=None, transient=False):
+class FormCreator():
+    def __init__(self):
+        pass
+    #@return
+    #poly - an integer between 1 and 9
+    #inflows - an array of spacial filters
+    #x_vels - an array of Functions for the x velocities mapping to inflows of the same array index
+    #y_vels - an array of Functions for the y velocities mapping to inflows of the same array index
+    #outflows - an array of spacial filters
+    #dims - the dimensions of the mesh; a list of doubles of length 2
+    #elems - a list of ints; the elements in the mesh
+    #re - renolds number, a double, optional argument
+    #transient - Boolean, whether or not the function is transient
+    def main(self, poly, inflows, x_vels, y_vels, outflows, dims, elems, re=None, transient=False):
         delta_k = 1 
         space_dim = 2
         mesh = self.makeMesh(dims, elems)
 
-        #for unloaded Navier
+        #for Navier
         if re != None:
             form = NavierStokesVGPFormulation(mesh, re, poly, delta_k)
     
-    #for unloaded Stokes
+    #for Stokes
         else:
             #self.context.setNav(False)
-            use_conforming_traces = False
+            use_conforming_traces = True
             mu = 1.0
-            transient = self.is_transient()
             form = StokesVGPFormulation(space_dim, use_conforming_traces, mu, transient)
-            """if self.context.loaded:
-                form.initializeSolution(self.context.file_name, poly_order)
-            else:"""
             form.initializeSolution(mesh, poly, delta_k)
-            form.addZeroMeanPressureCondition()
+        
+        form.addZeroMeanPressureCondition()
 
         #adding conditions
-        inflow = self.add_inflow_conditions(form, transient, inflows, inflow_x_vels, inflow_y_vels)
+        inflow = self.add_inflow_conditions(form, transient, inflows, x_vels, y_vels)
         outflow = self.add_outflow_conditions(form, outflows)
         #self.add_conditions("wall", form, transient)
-        form.addWallCondition(self.implicit_walls(dimensions, inflow, outflow))
+        form.addWallCondition(self.implicit_walls(dims, inflow, outflow))
+        
+        
+        return form
+
+        
 
 
         
         #if not loading from a file make a new mesh Topology
         #if not self.context.loaded:
-        def makeMesh(self, dims, elems):
-            x0 = [0., 0.]
-            return MeshFactory.rectilinearMeshTopology(dims, elems, x0)
-
-            #if self.loaded:
-                #NavierStokesVGPFormulation(self.context.file_name, space_dim, re, poly_order)
+    def makeMesh(self, dims, elems):
+        x0 = [0., 0.]
+        return MeshFactory.rectilinearMeshTopology(dims, elems, x0)
             
 
-        #solves and prints result
-        if is_Navier:
-            nonlinearThreshold = 1e-3
-            maxSteps = 10
-            normOfIncrement = 1
-            stepNumber = 0
-            while normOfIncrement > nonlinearThreshold and stepNumber < maxSteps:
-                form.solveAndAccumulate()
-                normOfIncrement = form.L2NormSolutionIncrement()
-                stepNumber += 1
-            mesh = form.solution().mesh()
-            energy = form.solutionIncrement().energyErrorTotal()
-            self.context.printRefine(energy, mesh)
 
-        else:
-            form.solve()
-            mesh = form.solution().mesh()
-            energy = form.solution().energyErrorTotal()
-            self.context.printRefine(energy, mesh)
        
-
-
-
 
 
     #adds wall/inflow/outflow conditions to the form
@@ -78,7 +65,7 @@ class FormCreator(State):
         for x in range(0, inflows.__len__()):
             velocity = self.get_velocity(transient, form, x_vels[x], y_vels[x])
             form.addInflowCondition(inflows[x], velocity)
-            total_boundary = SpatialFilter.unionFilter(total_boundary, boundary)
+            total_boundary = SpatialFilter.unionFilter(total_boundary, inflows[x])
         return total_boundary
 
     #adds wall/inflow/outflow conditions to the form
@@ -89,38 +76,23 @@ class FormCreator(State):
             total_boundary = SpatialFilter.unionFilter(total_boundary, boundary)
         return total_boundary
 
+    #adds wall conditions on all part of perameter that is not an inflow or outflow
     def implicit_walls(self, dimensions, inflow, outflow):
         flows = SpatialFilter.unionFilter(inflow, outflow)
-        x = dimensions[0]
-        y = dimensions[1]
-        leftWall = SpatialFilter.intersectionFilter(SpatialFilter.intersectionFilter(SpatialFilter.matchingX(0), SpatialFilter.lessThanY(y)), SpatialFilter.greaterThanY(0))
-        rightWall = SpatialFilter.intersectionFilter(SpatialFilter.intersectionFilter(SpatialFilter.matchingX(x), SpatialFilter.lessThanY(y)), SpatialFilter.greaterThanY(0))
-        topWall = SpatialFilter.intersectionFilter(SpatialFilter.intersectionFilter(SpatialFilter.matchingY(y), SpatialFilter.lessThanX(x)),SpatialFilter.greaterThanX(0))
-        bottumWall = SpatialFilter.intersectionFilter(SpatialFilter.intersectionFilter(SpatialFilter.matchingY(0), SpatialFilter.lessThanX(x)), SpatialFilter.greaterThanX(0))
-        perimeter = SpatialFilter.unionFilter(SpatialFilter.unionFilter(SpatialFilter.unionFilter(leftWall, rightWall), bottumWall), topWall)
-        wallConditions = SpatialFilter.unionFilter(perimeter, SpatialFilter.negatedFilter(flows)) 
+        wallConditions = SpatialFilter.negatedFilter(flows)
         return wallConditions
 
     #prototype
     def get_velocity(self, transient, form, x_vel, y_vel):
-        interp = Interpreter2()
-        xVelocity = interp.interpret(x_vel)
-        yVelocity = interp.interpret(y_vel)
-        topVelocity = Function.vectorize(xVelocity,yVelocity)
+        topVelocity = Function.vectorize(x_vel, y_vel)
         if transient:
             timeRamp = TimeRamp.timeRamp(form.getTimeFunction(),1.0)
             topVelocity = topVelocity*timeRamp
         return topVelocity
     
-    def get_velocity(self, string):
-        interp = Interpreter2()
-	try:
-        	x = interp.interpret(string)
-    	except NameError:
-        	print("Input Error")
-    	return x 
         
     #takes a string and returns a spacial filter
+    #unused for phase 3
     def get_space_fil(self, prompt):
         answer = self.context.query(prompt)
         altered = answer.lower()
